ec1bcd4a37c7840d7070d1b7733f81b5c021030b
patrick wai <weizhao1203@gmail.com>
2015-08-31 15:07:10 +0800

    rename FeedBackActivity.java to FeedbackActivity.java :

--- vcanpay/src/main/java/com/vcanpay/activity/help/FeedBackActivity.java
+++ vcanpay/src/main/java/com/vcanpay/activity/help/FeedBackActivity.java
@@ -9,34 +9,12 @@ import com.example.vcanpay.R;
 /**
  * Created by patrick wai on 2015/6/5.
  */
-public class FeedBackActivity extends ActionBarActivity {
+public class FeedbackActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_feed_back, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
